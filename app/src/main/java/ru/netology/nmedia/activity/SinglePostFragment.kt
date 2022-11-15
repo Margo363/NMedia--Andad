d920fe0cca4575097b87b2ddd20ed4e05ed3d0b1
Margo363 <anisimova.post@gmail.com>
2022-11-15 19:05:37 +0300

    changes made to files «1.1. Dependency Injection» and «1.2. Architecture Components. Часть 1»

--- app/src/main/java/ru/netology/nmedia/activity/SinglePostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/SinglePostFragment.kt
@@ -19,6 +19,7 @@ import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.utils.Utils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
+@AndroidEntryPoint
 class SinglePostFragment : Fragment() {
 
     @ExperimentalCoroutinesApi
@@ -50,58 +51,58 @@ class SinglePostFragment : Fragment() {
                     }
                 }
 
-            with(binding){
-                author.text = singlePost?.author
-                content.text = singlePost?.content
-                published.text = singlePost?.published
+                with(binding) {
+                    author.text = singlePost?.author
+                    content.text = singlePost?.content
+                    published.text = singlePost?.published
 
-                like.text = singlePost?.let { it -> Utils.reductionInNumbers(it.likes) }
-                share.text = singlePost?.sharesCount?.let { it ->
-                    Utils.reductionInNumbers(
-                        it
-                    )
-                }
-                like.isChecked = singlePost?.likedByMe == true
-                if (singlePost?.video != "") binding.group.visibility = View.VISIBLE
-                if (singlePost?.attachment != null) viewForImage.visibility = View.VISIBLE
-
-
-            }
-
-        binding.menu.setOnClickListener {
-            PopupMenu(it.context, it).apply {
-                inflate(R.menu.post_options)
-                setOnMenuItemClickListener { menuItem ->
-                    when (menuItem.itemId) {
-                        R.id.post_remove -> {
-
-                            singlePost?.id?.let { id -> viewModel.removeById(id) }
-
-                            findNavController().navigate(
-                                R.id.action_singlePostFragment_to_feedFragment
-                            )
-                            true
-                        }
-                        R.id.post_edit -> {
-
-                            singlePost?.let { it -> viewModel.edit(it) }
-
-                            bundle.putString("content", singlePost?.content)
-
-                            findNavController().navigate(
-                                R.id.action_singlePostFragment_to_newPostFragment,
-                                bundle
-                            )
-                            true
-                        }
-                        else -> false
+                    like.text = singlePost?.let { it -> Utils.reductionInNumbers(it.likes) }
+                    share.text = singlePost?.sharesCount?.let { it ->
+                        Utils.reductionInNumbers(
+                            it
+                        )
                     }
-                }
-            }.show()
-        }
+                    like.isChecked = singlePost?.likedByMe == true
+                    if (singlePost?.video != "") binding.group.visibility = View.VISIBLE
+                    if (singlePost?.attachment != null) viewForImage.visibility = View.VISIBLE
 
+
+                }
+
+                binding.menu.setOnClickListener {
+                    PopupMenu(it.context, it).apply {
+                        inflate(R.menu.post_options)
+                        setOnMenuItemClickListener { menuItem ->
+                            when (menuItem.itemId) {
+                                R.id.post_remove -> {
+
+                                    singlePost?.id?.let { id -> viewModel.removeById(id) }
+
+                                    findNavController().navigate(
+                                        R.id.action_singlePostFragment_to_feedFragment
+                                    )
+                                    true
+                                }
+                                R.id.post_edit -> {
+
+                                    singlePost?.let { it -> viewModel.edit(it) }
+
+                                    bundle.putString("content", singlePost?.content)
+
+                                    findNavController().navigate(
+                                        R.id.action_singlePostFragment_to_newPostFragment,
+                                        bundle
+                                    )
+                                    true
+                                }
+                                else -> false
+                            }
+                        }
+                    }.show()
+                }
+            }
+        }
 
         return binding.root
     }
-
 }
